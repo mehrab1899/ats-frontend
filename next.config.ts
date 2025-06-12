f52fdedf6c3ca6e74643b9f29e2aba4acd76119b
mehrab1899 <mehrabmehmood1899@gmail.com>
2025-06-12 16:28:55 +0100

    Push folder structure and initial layout setup to development branch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true, // Enable the App Router
+  },
 };
 
 export default nextConfig;
